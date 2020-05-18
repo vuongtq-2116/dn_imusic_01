55e3e83554042c16725bbf847ce56b3f44042a21
vuongtq-2116 <thai.quoc.vuong@sun-asterisk.com>
2020-05-18 15:09:11 +0700

    static page and nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
 
+    get "/signup", to: "users#new"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
     resources :users
   end
 end
